df10f806eb93f23e048bdfa4b794fd211c333740
rigo5632 <rmquiroz5@gmail.com>
2020-11-02 18:43:27 -0700

    added consumer and producer class

--- Video-Player/Player.py
+++ Video-Player/Player.py
@@ -1,30 +1,46 @@
 #! /usr/bin/python3
-from threading import Thread, Semaphore
+from threading import Thread, Semaphore, Lock
 import cv2, time
 
-semaphore = Semaphore(2)
-frameQueue = []
-grayScaleQueue = []
-
+class producerConsumerQueue():
+    def __init__(self, queueCapacity):
+        self.queue = []
+        self.fullCount = Semaphore(0)
+        self.emptyCount = Semaphore(24)
+        self.lock = Lock()
+        self.queueCapacity = queueCapacity
+        
+    def insertFrame(self, frame):
+        self.emptyCount.acquire()
+        self.lock.acquire()
+        self.queue.append(frame)
+        self.lock.release()
+        self.fullCount.release()
+        return
+    
+    def getFrame(self):
+        self.fullCount.acquire()
+        self.lock.acquire()
+        frame = self.queue.pop(0)
+        self.lock.release()
+        self.emptyCount.release()
+        return frame
+    
 class ExtractFrames(Thread):
-    def __init__(self):
+    def __init__(self):  
         Thread.__init__(self)
         self.videoCapture = cv2.VideoCapture('clip.mp4')
         self.totalFrames = int(self.videoCapture.get(cv2.CAP_PROP_FRAME_COUNT))
-        self.queueCapacity = 50
         self.count = 0
         
     def run(self):
         global frameQueue #frame queue
-        global semaphore #lock
         success, image = self.videoCapture.read()
 
         while True:
             # if we have an image and our frame queue is not full: add frame
-            if success and len(frameQueue) <= self.queueCapacity:
-                semaphore.acquire() #lock
-                frameQueue.append(image)
-                semaphore.release() #lock
+            if success and len(frameQueue.queue) <= frameQueue.queueCapacity: 
+                frameQueue.insertFrame(image)
 
                 success, image = self.videoCapture.read() #get next frame from file
                 print(f'Reading frame {self.count}')
@@ -32,46 +48,34 @@ class ExtractFrames(Thread):
 
             # we have acquired all frames
             if self.count == self.totalFrames:
-                semaphore.acquire()
-                frameQueue.append(-1) #append end key
-                semaphore.release()
+                frameQueue.insertFrame(-1)
                 break            
         return
             
 class ConvertToGrayScale(Thread):
         def __init__(self):
             Thread.__init__(self)
-            self.queueCapacity = 50
             self.count = 0
             
         def run(self):
             global frameQueue #frame queue
-            global grayScaleFrame #grayscale frame queue
-            global semaphore #lock
+            global grayScaleQueue #grayscale frame queue
             
             while True:
                 #if we have frames in our queue and our queue is not full
-                if len(frameQueue) >= 1 and len(grayScaleQueue) <= self.queueCapacity:
-                    semaphore.acquire()
-                    frame = frameQueue.pop(0) #pop frame from queue1
-                    semaphore.release()
+                if frameQueue.queue and len(grayScaleQueue.queue) <= grayScaleQueue.queueCapacity:
+                    frame = frameQueue.getFrame()
 
                     # if we see end key stop appending gray frames, exit thread
                     if type(frame) == int and frame == -1:
-                        semaphore.acquire()
-                        grayScaleQueue.append(-1)
-                        semaphore.release()
+                        grayScaleQueue.insertFrame(-1)
                         break
                     
                     #get frame from queue1, convert it and append it to queue 2
                     print(f'Converting Frame {self.count}')
                     grayscaleFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                    semaphore.acquire()
-                    grayScaleQueue.append(grayscaleFrame)
-                    semaphore.release()
+                    grayScaleQueue.insertFrame(grayscaleFrame)
                     self.count += 1
-                else:
-                    time.sleep(0.10) # sleep if we don't need to do work
             return
 
 class ShowMovie(Thread):
@@ -82,14 +86,11 @@ class ShowMovie(Thread):
 
     def run(self):
         global grayScaleQueue #grayscale queue2
-        global semaphore #lock
 
         while True:
             #as long as we have frames in our queue
-            if len(grayScaleQueue) >= 1:
-                semaphore.acquire()
-                frame = grayScaleQueue.pop(0) #pop frame from queue2
-                semaphore.release()
+            if grayScaleQueue.queue:
+                frame = grayScaleQueue.getFrame()
 
                 # if we see the end key, exit display thread
                 if type(frame) == int and frame == -1:
@@ -101,14 +102,15 @@ class ShowMovie(Thread):
                 self.count += 1
 
                 #exit key
-                if cv2.waitKey(self.delay) and 0xFF == ord('q'):
+                if cv2.waitKey(42) and 0xFF == ord('q'):
                     break
-            else:
-                time.sleep(0.10) #sleep if we don't need to do work
                     
         #destory video screen
         cv2.destroyAllWindows()
         return
+
+frameQueue = producerConsumerQueue(9)
+grayScaleQueue = producerConsumerQueue(9)
 
 extractFrames = ExtractFrames()
 extractFrames.start()
@@ -116,8 +118,6 @@ convertFrames = ConvertToGrayScale()
 convertFrames.start()
 displayFrames = ShowMovie()
 displayFrames.start()
-
-
 
 
 
